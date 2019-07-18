2f3c9af59afaa7fb256dc9926d55884f470f63f1
Taisuke Fukuno <fukuno@jig.jp>
2019-07-18 14:15:14 +0900

    add src link

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <Events/>
     </v-content>
     <div class=footer>
-      <div>App: by <a href=https://code4fukui.github.io/fukuigc/>Code for Fukui</a></div>
+      <div>App: by <a href=https://code4fukui.github.io/>Code for Fukui</a> (<a href=https://github.com/code4fukui/SaitamaEvents>src on GitHub</a>)</div>
       <div>Base App: <a href=https://watano1168.github.io/FukuiEvents/>福井県のイベント</a> (<a href=https://github.com/watano1168/FukuiEvents>src on GitHub</a>)</div>
     </div>
   </v-app>
